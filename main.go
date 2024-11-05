f9f94da903d84fb58101201234913d04fd238af4
Marcel Bednarczyk <marcelbednarczyk@onet.pl>
2024-11-05 21:57:52 +0100

    Get Default Info - request

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "log/slog"
+import (
+	"log/slog"
+
+	"github.com/marcelbednarczyk/Golang-Dual-EEG/cortex"
+)
 
 func main() {
 	slog.Info("Hello, World!")
+	_ = cortex.GetDefaultInfoRequest()
 }
