56a79e769d04617b4a5cb1fad820aa50a561f127
Marcel Bednarczyk <marcelbednarczyk@onet.pl>
2024-12-08 16:21:58 +0100

    working progress

--- server/cortex/Listen.go
+++ server/cortex/Listen.go
@@ -1,13 +1,13 @@
 package cortex
 
 import (
+	"context"
 	"log/slog"
-	"time"
 
 	"golang.org/x/net/websocket"
 )
 
-func Listen(ws *websocket.Conn, token, headsetId, name string) error {
+func Listen(ctx context.Context, ws *websocket.Conn, token, headsetId, name string, ch chan<- float64) error {
 	if err := Send(ws, GetOpenSessionRequest(token, headsetId)); err != nil {
 		return err
 	}
@@ -24,53 +24,22 @@ func Listen(ws *websocket.Conn, token, headsetId, name string) error {
 		return err
 	}
 
-	// f, err := os.Create(name + ".csv")
-	// if err != nil {
-	// 	return err
-	// }
-	// defer f.Close()
-	// if _, err := f.WriteString(`"P1 alpha";"P1 low beta";"P1 high beta";"P1 gamma";"P1 theta";"P2 alpha";"P2 low beta";"P2 high beta";"P2 gamma";"P2 theta";"P3 alpha";"P3 low beta";"P3 high beta";"P3 gamma";"P3 theta";"P4 alpha";"P4 low beta";"P4 high beta";"P4 gamma";"P4 theta";"P5 alpha";"P5 low beta";"P5 high beta";"P5 gamma";"P5 theta"`); err != nil {
-	// 	return err
-	// }
-	// if _, err := f.WriteString("\n"); err != nil {
-	// 	return err
-	// }
-
-	sum, count := 0.0, 0
-	ticker := time.NewTicker(10 * time.Second)
 	for {
 		select {
-		case <-ticker.C:
-			slog.Info("Average score", slog.Float64("score", sum/float64(count)))
-			return nil
+		case <-ctx.Done():
+			return ctx.Err()
 		default:
 			var data SubscribeData
 			if err := Receive(ws, &data); err != nil {
 				return err
 			}
-			// for _, p := range data.Pow {
-			// 	if _, err := f.WriteString(fmt.Sprintf("%f;", p)); err != nil {
-			// 		return err
-			// 	}
-			// }
-			// if _, err := f.WriteString("\n"); err != nil {
-			// 	return err
-			// }
 
 			score, err := calculateScore(data.Pow)
 			if err != nil {
 				slog.Warn("Error calculating score", slog.String("error", err.Error()))
 				continue
 			}
-			sum += score
-			count++
+			ch <- score
 		}
 	}
 }
-
-// var x map[string]interface{}
-// if err := Receive(ws, &x); err != nil {
-// 	return err
-// }
-// jsonString, _ := json.Marshal(x)
-// fmt.Println(string(jsonString))
--- server/main.go
+++ server/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
+	"encoding/json"
 	"log/slog"
+	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/marcelbednarczyk/Golang-Dual-EEG/cortex"
 	"golang.org/x/net/websocket"
+)
+
+const (
+	numberOfHeadsets = 2
 )
 
 func main() {
@@ -25,22 +34,107 @@ func main() {
 		}
 	}
 
-	ws, err := websocket.DialConfig(config)
-	if err != nil {
-		slog.Error("Error dialing websocket", slog.String("error", err.Error()))
-		return
+	websockets := make([]*websocket.Conn, numberOfHeadsets)
+	for i := 0; i < numberOfHeadsets; i++ {
+		ws, err := websocket.DialConfig(config)
+		if err != nil {
+			slog.Error("Error dialing websocket", slog.String("error", err.Error()))
+			return
+		}
+		websockets[i] = ws
 	}
 
-	token, headsetId, err := cortex.ConnectHeadset(ws, os.Getenv("HEADSET_NAME"), os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"))
-	if err != nil {
-		slog.Error("Error connecting headset", slog.String("error", err.Error()))
-		return
+	scoreChans := make([]chan float64, numberOfHeadsets)
+	for i := 0; i < numberOfHeadsets; i++ {
+		scoreChans[i] = make(chan float64)
 	}
-	slog.Info("Connected to headset", slog.String("headset", headsetId))
 
-	if err := cortex.Listen(ws, token, headsetId, os.Getenv("HEADSET_NAME")); err != nil {
-		slog.Error("Error subscribing to headset", slog.String("error", err.Error()))
-		return
+	ctx, cancel := context.WithTimeout(context.Background(), 1000*time.Second)
+	defer cancel()
+
+	clientId, clientSecret := os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET")
+	for i := 0; i < numberOfHeadsets; i++ {
+		headsetName := os.Getenv("HEADSET_NAME_" + strconv.Itoa(i+1))
+		go func(headsetName, clientId, clientSecret string) {
+			token, headsetId, err := cortex.ConnectHeadset(websockets[i], headsetName, clientId, clientSecret)
+			if err != nil {
+				slog.Error("Error connecting headset", slog.String("headsetName", headsetName), slog.String("error", err.Error()))
+				return
+			}
+			slog.Info("Connected to headset", slog.String("headsetName", headsetName), slog.String("headsetId", headsetId))
+
+			if err := cortex.Listen(ctx, websockets[i], token, headsetId, headsetName, scoreChans[i]); err != nil {
+				slog.Error("Error subscribing to headset", slog.String("headsetName", headsetName), slog.String("error", err.Error()))
+				return
+			}
+			slog.Info("Stopped", slog.String("headsetName", headsetName), slog.String("headsetId", headsetId))
+		}(headsetName, clientId, clientSecret)
+	selectLoop:
+		for {
+			select {
+			case score := <-scoreChans[i]:
+				slog.Info("Score", slog.Float64("score", score))
+				break selectLoop
+			default:
+				continue
+			}
+		}
 	}
-	slog.Info("Stopped")
+
+	value := 50 // %
+	go func() {
+		for {
+			time.Sleep(100 * time.Millisecond)
+			score1, score2 := tryReceive(scoreChans[0]), tryReceive(scoreChans[1])
+			if score1 == 0 && score2 == 0 {
+				continue
+			}
+			if score1 > score2 {
+				slog.Info("Score 1 is higher", slog.Float64("score1", score1), slog.Float64("score2", score2))
+				value++
+			} else {
+				slog.Info("Score 2 is higher", slog.Float64("score1", score1), slog.Float64("score2", score2))
+				value--
+			}
+		}
+	}()
+
+	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		switch r.URL.Path {
+		case "/api/dual-eeg/v1.0/score":
+			switch r.Method {
+			case http.MethodGet:
+				score := ScoreResponse{Score: value}
+				resp, err := json.Marshal(score)
+				if err != nil {
+					http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
+					return
+				}
+				w.Header().Set("Content-Type", "application/json")
+				w.Write(resp)
+			case http.MethodOptions:
+				w.WriteHeader(http.StatusOK)
+			default:
+				http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			}
+		default:
+			http.NotFound(w, r)
+		}
+	}))
+}
+
+type ScoreResponse struct {
+	Score int `json:"score"`
+}
+
+func tryReceive(ch <-chan float64) float64 {
+	select {
+	case score := <-ch:
+		return score
+	default:
+		return 0
+	}
 }
